8fff6a0f8cfcc43ff46eee54c6dc636ec912e034
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-03-04 12:25:29 +0100

    c

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,48 @@ export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     console.log('issue was opened!')
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for opening this issue!"
     });
-    return context.octokit.issues.createComment(issueComment);
+    context.octokit.issues.createComment(issueComment);
+    context.octokit.issues.addLabels({
+      owner: context.issue().owner,
+      repo: context.issue().repo,
+      issue_number: context.issue().issue_number,
+      labels: ["Bot Test", "invalid"]
+    })
+    context.octokit.issues.create({
+      owner: context.issue().owner, 
+      repo: context.issue().repo, 
+      title: "Hehe, cummit incumming",
+      body: "Yes, the time has cum"
+    })
+    
+    context.octokit.reactions.createForIssueComment({
+      owner: context.issue().owner,
+      repo: context.issue().repo, 
+      comment_id: 1159522839,
+      content: "heart"
+    })
+    
+    return 
+    
   });
-  app.on("push", async (ctx) => {
-    console.log('PUSSSSHSHHHHH')
-    return ctx.octokit.repos.createCommitComment({ owner: "ArvidWedtstein", repo: "Github-Bot", commit_sha: "e40c051", body: `Thanks for pushing ${ctx.payload.pusher.name}!`})
-  })
+  app.on("push", async (context) => {
+    // Code was pushed to the repo, what should we do with it?
+    app.log.info(context);
+  });
   app.on("commit_comment.created", async (ctx) => {
     console.log('commit comment was created!!!')
-    return ctx.octokit.issues.create({owner: "ArvidWedtstein", repo: "Github-Bot", title: "Hehe, cummit incumming"})
-  })
-  app.on("issue_comment.created", async (ctx) => {
-    console.log('Issue comment was created')
-    
-    ctx.octokit.issues.addLabels({
-      owner: ctx.issue().owner,
-      repo: ctx.issue().repo,
-      issue_number: ctx.issue().issue_number,
-      labels: [{name: "Bot Test"}]
-    })
-    return ctx.octokit.projects.createForRepo({
-      owner: "ArvidWedtstein",
-      repo: "Github-Bot",
-      name: "Test Project"
+    return ctx.octokit.issues.create({
+      owner: ctx.issue().owner, 
+      repo: ctx.issue().repo, 
+      title: "Hehe, cummit incumming",
+      body: "Yes, the time has cum"
     })
   })
+  app.onAny(async (context) => {
+    app.log.info({ event: context.name, action: context.payload.action });
+  });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
