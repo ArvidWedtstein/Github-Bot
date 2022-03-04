aad635922c12687889568c854180b4241f66cf0b
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-03-04 11:46:15 +0100

    d

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Probot } from "probot";
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
+    console.log('issue was opened!')
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
@@ -12,6 +13,7 @@ export = (app: Probot) => {
     return ctx.octokit.repos.createCommitComment({ owner: "ArvidWedtstein", repo: "Github-Bot", commit_sha: "e40c051", body: `Thanks for pushing ${ctx.payload.pusher.name}!`})
   })
   app.on("commit_comment.created", async (ctx) => {
+    console.log('commit comment was created!!!')
     return ctx.octokit.issues.create({owner: "ArvidWedtstein", repo: "Github-Bot", title: "Hehe, cummit incumming"})
   })
   // For more information on building apps:
